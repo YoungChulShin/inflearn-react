6903ad10f1090846871f2f07b85c52e33efbdd01
YoungChulShin <go1323@gmail.com>
2023-04-10 23:22:29 +0900

    jsx 기초

--- reactexam1/src/App.js
+++ reactexam1/src/App.js
@@ -1,15 +1,31 @@
 import './App.css';
+import MyHeader from './MyHeader';
+import MyFooter from './MyFooter';
+import React from 'react';
 
 function App() {
 
   let name = "테스트 사용자";
 
+  const func = () => {
+    return "func";
+  }
+
+  const number = 5;
+
   return (
+    // 최상위 태그
     <div className="App">
-      <header className="App-header">
-        <h2>안녕 리액트 {name}</h2>
-      </header>
+      <MyHeader />
+      <h2>안녕 리액트 {name}</h2>
+      <b id="bold_text">
+        {number}는 : {number % 2 === 0 ? "짝수" : "홀수"}
+      </b>
     </div>
+
+    // React.Fargment로 나눌 수 있다
+    // <React.Fragment>
+    // </React.Fragment>
   );
 }
 
