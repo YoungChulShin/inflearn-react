58930ad8913a96f9659c39ce7c848e201d882f25
YoungChulShin <go1323@gmail.com>
2023-04-11 21:25:32 +0900

    state

--- reactexam1/src/App.js
+++ reactexam1/src/App.js
@@ -2,30 +2,14 @@ import './App.css';
 import MyHeader from './MyHeader';
 import MyFooter from './MyFooter';
 import React from 'react';
+import Counter from './Counter';
 
 function App() {
-
-  let name = "테스트 사용자";
-
-  const func = () => {
-    return "func";
-  }
-
-  const number = 5;
-
   return (
-    // 최상위 태그
-    <div className="App">
+    <div>
       <MyHeader />
-      <h2>안녕 리액트 {name}</h2>
-      <b id="bold_text">
-        {number}는 : {number % 2 === 0 ? "짝수" : "홀수"}
-      </b>
+      <Counter />
     </div>
-
-    // React.Fargment로 나눌 수 있다
-    // <React.Fragment>
-    // </React.Fragment>
   );
 }
 
