d6f3c434e93e17b9a7df7d3ecf38ad5c6c80149f
YoungChulShin <go1323@gmail.com>
2023-04-28 06:33:13 +0900

    emotiondiary: router 기능 옵션 추가

--- emotiondiary/src/pages/Edit.js
+++ emotiondiary/src/pages/Edit.js
@@ -1,9 +1,27 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 const Edit = () => {
+
+    const navigate = useNavigate();
+
+    const [searchParamas, setSearchParams] = useSearchParams();
+    const id = searchParamas.get("id");
+    console.log(id);
+
+    const mode = searchParamas.get("mode");
+    console.log(mode);
+
     return (
         <div>
             <h1>Edit</h1>
             <p>이곳은 일치 수정 페이지 입니다</p>
+            <button onClick={() => setSearchParams({who: "youngchulshin"})}>QS 바꾸기</button>
+
+            <button onClick={() => navigate("/HOME")}>HOME으로 가기</button>
+            <button onClick={() => navigate(-1)}>뒤로 가기</button>
+            
         </div>
+    
     );
 }
 
