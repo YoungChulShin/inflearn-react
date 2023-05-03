dd61b9a8a4ef2405e2b4cac91c5575515302abca
YoungChulShin <go1323@gmail.com>
2023-05-04 00:29:35 +0900

    emotiondiary: DiaryList 추가

---
+++ emotiondiary/src/components/DiaryList.js
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+
+const DiaryList = ({ diaryList }) => {
+    return (
+        <div>
+            {diaryList.map((it) => (
+                <div key={it.id}>{it.content}</div>
+            ))}
+        </div>);
+}
+
+DiaryList.defaultProps = {
+    diaryList: [],
+};
+
+export default DiaryList;
